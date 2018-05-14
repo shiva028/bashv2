9afc48dc8234a7e6b9eea349f6a4b474ab7588c6
root <root@server.corp.ncodeit.com>
2018-05-14 16:45:24 +0530

    addeded

---
+++ function.sh
@@ -0,0 +1,12 @@
+simple function example
+#.............................
+func()
+{
+	echo "inside function"
+}
+
+#................................
+echo "now function call.."
+func
+echo "end of main"
+
