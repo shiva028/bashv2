ed7699891bb5cdb82071afc16501132492096d69
root <root@server.corp.ncodeit.com>
2018-05-14 17:04:34 +0530

    parameters to function

---
+++ parameters.sh
@@ -0,0 +1,18 @@
+# pass of parameters to function
+#---------------------------------------------------
+#!/bin/sh
+
+#---------------------------------
+func()
+{
+	echo "We are in function now"
+	echo $0  # shell script name
+	echo $1  # first parameter
+	echo $2  # second parameter
+	echo "We leave function..."
+	exit 0
+}
+
+#---------------------------------
+
+func  123  "abc"
