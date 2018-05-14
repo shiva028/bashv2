91856536dd7e9b516fd04e235a993a9e35294ae1
root <root@server.corp.ncodeit.com>
2018-05-14 15:16:35 +0530

    added

---
+++ interger_bash.sh
@@ -0,0 +1,9 @@
+# integer arithmetics - bash only !
+#---------------------------------------------------
+#!/bin/bash
+
+echo 'number=?' ; read x
+let y=$x+$x ; echo 'x+x=' $y
+let y=$*$x ; echo 'square=` $y
+let y=$x/3 ; echo `x/3=` $y
+let y=$x%7 ; echo `x%7=` $y
