406e2cd7b23f490edeae5c2927dba35769806f4e
root <root@server.corp.ncodeit.com>
2018-05-14 16:49:03 +0530

    addeded

---
+++ function1.sh
@@ -0,0 +1,14 @@
+function can see variables of main program
+............................................
+
+#.......................................
+func()
+{
+	echo $var
+
+}
+#.........................
+
+var="test of global "
+func
+
