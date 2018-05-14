3a56983879c4cf9775b4579a7a7bf1edcde637f9
root <root@server.corp.ncodeit.com>
2018-05-14 23:14:46 +0530

    dynamic expansion of arry

---
+++ dynamic.sh
@@ -0,0 +1,27 @@
+arr=()
+
+n=${#arr[@]}
+echo "number of array elements "$n
+
+arr[0]=a
+n=${#arr[@]}
+echo "number of array elements "$n
+
+arr[1]=b
+n=${#arr[@]}
+echo "number of array elements "$n
+
+arr[2]=c
+n=${#arr[@]}
+echo "number of array elements "$n
+
+arr[10]=h
+n=${#arr[@]}
+echo "number of array elements "$n
+
+
+echo ${arr[10]}
+
+echo ${arr[4]} # empty string
+echo ${arr[6]} # empty string
+
